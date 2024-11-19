71474e6a2dd06123ff51aa9f5f82825349d53965
Oktawian Bieszke <oktawianbieszke7@gmail.com>
2024-11-19 13:24:52 +0100

    Squashed commit of the following:
    commit 09c7a9dd2895262bcd997d696bdfad999d1c4c6e
    Author: Oktawian Bieszke <oktawianbieszke7@gmail.com>
    Date:   Tue Nov 19 12:34:30 2024 +0100
    
        player is no more able to escape out of window
    
    commit 02e000eadca7054668547b0899ce916258a86cd3
    Author: Oktawian Bieszke <oktawianbieszke7@gmail.com>
    Date:   Mon Nov 18 22:51:50 2024 +0100
    
        created separate function that handles movement
    
        added Quit option, that is 'x'
    
    commit 83f55196e1d50cba5f37e8586a68cf0aeafd28d2
    Author: Oktawian Bieszke <oktawianbieszke7@gmail.com>
    Date:   Mon Nov 18 22:22:29 2024 +0100
    
        gi
    
    commit 1c7d52129415bcda7adac165ae27e66369bcd199
    Author: Oktawian Bieszke <oktawianbieszke7@gmail.com>
    Date:   Mon Nov 18 22:20:00 2024 +0100
    
        changed a Start function name and added some comments

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
-#define MAIN_COLOR 1
+#define FROG_COLOR 1
 
 #define mapHeight 30
-#define mapWidth 59
+#define mapWidth 30
 #define mapX 0
 #define mapY 0
 
-#define carCount 
 
-typedef enum{
-    player,
-    car,
-}OBJECTTYPE;
 
 //------------------------------------------------
 //-------------------- STRUCTS -------------------
@@ -32,20 +27,31 @@ typedef struct{
     int height,width;
     char shape;
     int dir;
-    OBJECTTYPE type;
     bool isHidden;
     bool isOverlapping;
 }OBJECT;
 
+typedef struct{
+    int x,y;
+    int color;
+    int maxX, maxY, minX, minY;
+    int height,width;
+    char shape;
+}PLAYER;
+
+
+//------------------------------------------------
+//----------------- INIT FUNCTIONS ---------------
+//------------------------------------------------
 
 //function initializng main window
-WINDOW * Start(){
+WINDOW * InitGame(){
     WINDOW * stdwin;
     stdwin = initscr();
 
 
     start_color();
-    init_pair(1,COLOR_WHITE,COLOR_GREEN);
+    init_pair(FROG_COLOR,COLOR_WHITE,COLOR_GREEN);
 
     
     cbreak();
@@ -54,14 +60,15 @@ WINDOW * Start(){
     return stdwin;
 }
 
-OBJECT* InitPlayer(int y, int x,int maxY,int minX,int maxX,char shape){
-    OBJECT * tempPlayer = (OBJECT*)malloc (sizeof(OBJECT));
+PLAYER* InitPlayer(int y, int x,int minY,int maxY,int minX,int maxX,char shape){
+    PLAYER * tempPlayer = (PLAYER*)malloc (sizeof(PLAYER));
     tempPlayer->y =y;
     tempPlayer->x =x;
+    //min and max of frogs position
+    tempPlayer->minY =minY;
     tempPlayer->maxY =maxY;
-    tempPlayer->maxX =maxY;
+    tempPlayer->maxX =maxX;
     tempPlayer->minX =minX;
-    tempPlayer->type = player;
     tempPlayer->shape = shape;
     return tempPlayer;
 }
@@ -78,42 +85,51 @@ WIN * InitWindow(WINDOW*parent, int width,int height,int y, int x){
     return tempWin;
 }
 
-
-int main(){
-    WINDOW*stdwin = Start();
-    WIN * map = InitWindow(stdwin,mapHeight,mapWidth,mapY,mapX);
-    OBJECT * player = InitPlayer(mapHeight-2,mapWidth/2,mapHeight,0,mapWidth,'O');
-    
-    char ch;
-    mvwprintw(map->win,0,3,"frogger nigger");
-
-    wattron(map->win, COLOR_PAIR(1));
-    mvwaddch(map->win,player->y,player->x,player->shape);
-    wattroff(map->win,COLOR_PAIR(1));
-    keypad(stdscr,TRUE);
-    while(1){
-        wrefresh(map->win);
-        while (ch=wgetch(map->win))
-        {
-            mvwaddch(map->win,player->y,player->x,' ');
-            if(ch == 'w'){
+void PlayerMovement(WINDOW*win,PLAYER*player, int ch,bool *isRunning){
+        if(ch=getch()){
+            mvwaddch(win,player->y,player->x,' ');
+            if(ch == 'w' && player->y>player->minY){
                 player->y-=1;       
             }
-            if(ch == 's'){
+            if(ch == 's'&& player->y<player->maxY){
                 player->y+=1;       
             }
-            if(ch == 'a'){
+            if(ch == 'a'&& player->x>player->minX){
                 player->x-=1;       
             }
-            if(ch == 'd'){
+            if(ch == 'd' && player->x<player->maxX){
                 player->x+=1;        
             }
-            wattron(map->win, COLOR_PAIR(1));
-            mvwaddch(map->win,player->y,player->x,player->shape);
-            wattroff(map->win,COLOR_PAIR(1)); 
+            if(ch=='x'){
+                *isRunning = false;
+            }
+            wattron(win, COLOR_PAIR(FROG_COLOR) | A_BOLD);
+            mvwaddch(win,player->y,player->x,player->shape);
+            wattroff(win,COLOR_PAIR(FROG_COLOR) | A_BOLD); 
         }
-    }
+}
 
+
+int main(){
+    
+    WINDOW*stdwin = InitGame();
+    WIN * map = InitWindow(stdwin,mapHeight,mapWidth,mapY,mapX);
+    PLAYER * player = InitPlayer(mapHeight-2,mapWidth/2,1,mapHeight-2,1,mapWidth-2,'O');
+    
+
+    mvwprintw(map->win,0,3,"frogger");
+    wattron(map->win, COLOR_PAIR(FROG_COLOR) | A_BOLD);
+    mvwaddch(map->win,player->y,player->x,player->shape);
+    wattroff(map->win,COLOR_PAIR(FROG_COLOR) | A_BOLD); 
+
+    //basic gameloop
+    int ch;
+    bool isRunning = true;
+    while(isRunning){
+        PlayerMovement(map->win,player,ch,&isRunning);
+        wrefresh(map->win);
+
+    }
 
     endwin();
     return 0;
